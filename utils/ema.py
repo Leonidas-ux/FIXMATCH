bb4df88b620e62c14faf3be58451498e8050389c
Leo <leo@Leos-MacBook-Pro.local>
2026-04-18 20:28:46 +0900

    Clean FixMatch updates without dataset artifacts

--- utils/ema.py
+++ utils/ema.py
@@ -1,38 +1,24 @@
+
+"""Exponential Moving Average (EMA) of model parameters."""
+import torch
 from copy import deepcopy
 
-import torch
 
-
-class ModelEMA(object):
-    def __init__(self, args, model, decay):
+class ModelEMA:
+    def __init__(self, model, decay=0.999):
         self.ema = deepcopy(model)
-        self.ema.to(args.device)
         self.ema.eval()
         self.decay = decay
-        self.ema_has_module = hasattr(self.ema, 'module')
-        # Fix EMA. https://github.com/valencebond/FixMatch_pytorch thank you!
-        self.param_keys = [k for k, _ in self.ema.named_parameters()]
-        self.buffer_keys = [k for k, _ in self.ema.named_buffers()]
+
         for p in self.ema.parameters():
             p.requires_grad_(False)
 
     def update(self, model):
-        needs_module = hasattr(model, 'module') and not self.ema_has_module
         with torch.no_grad():
             msd = model.state_dict()
             esd = self.ema.state_dict()
-            for k in self.param_keys:
-                if needs_module:
-                    j = 'module.' + k
+            for k in esd.keys():
+                if esd[k].dtype.is_floating_point:
+                    esd[k].mul_(self.decay).add_(msd[k].detach(), alpha=1 - self.decay)
                 else:
-                    j = k
-                model_v = msd[j].detach()
-                ema_v = esd[k]
-                esd[k].copy_(ema_v * self.decay + (1. - self.decay) * model_v)
-
-            for k in self.buffer_keys:
-                if needs_module:
-                    j = 'module.' + k
-                else:
-                    j = k
-                esd[k].copy_(msd[j])
+                    esd[k].copy_(msd[k])
